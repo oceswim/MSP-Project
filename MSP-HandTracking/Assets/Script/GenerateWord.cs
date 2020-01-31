c0cc005cad0d0969d2b1abb4a475424b917dd88b
ocesiwm <oceaneperetti@gmail.com>
2020-01-31 17:49:40 +0000

    automatising words and images display

--- MSP-HandTracking/Assets/Script/GenerateWord.cs
+++ MSP-HandTracking/Assets/Script/GenerateWord.cs
@@ -1,56 +1,228 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using TMPro;
-
+using UnityEngine.UI;
+using System.IO;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using System.Collections;
+/*
+*
+*AIzaSyAbZuzy5_ilRsP6xNHIyP4sBH7ixLBqsOw --> the API key
+*search engine id 003496967335787894975:7yarfcsjner
+*miel
+*
+*/
 public class GenerateWord : MonoBehaviour
 {
-    public TMP_Text englishWord, frenchWord,word1,word2,word3;
-    public Material frameTexture;
-    public Texture[] wordPicture;
-    
-    // Start is called before the first frame update
+    //using google API custom search to get images online based on the word
+    private const string URL = "https://www.googleapis.com/customsearch/v1?key=AIzaSyAbZuzy5_ilRsP6xNHIyP4sBH7ixLBqsOw&cx=003496967335787894975:7yarfcsjner&q=";
+
+    public TMP_Text englishWord, frenchWord, word1, word2, word3;
+    private List<string> englishVersion, frenchVersion;
+    public Material frameTexture;//applied based on the current word
+    public Texture[] colors;//will be fetched online
+    private string path = "Assets/Words/words.txt";
+    private string imgURL;
+    private int currentIndex;
     void Start()
     {
-        
+        currentIndex = 0;
+        englishVersion = new List<string>();
+        frenchVersion = new List<string>();
+
+
+        //based on level then fetch colors/ object / places
+        if (PlayerPrefs.GetInt("Level") > 1)
+        {
+            FillWordArray();
+            FetchContent(currentIndex);//will fetch the needed words and textures if level not one.
+        }
+        else
+        {
+            for(int i=0;i<colors.Length;i++)
+            {
+                englishVersion.Add(colors[i].name);
+               switch(colors[i].name)
+                {
+                    
+                    case "blue":
+                        frenchVersion.Add("bleu");
+                        break;
+                    case "green":
+                        frenchVersion.Add("green");
+                        break;
+                    case "purple":
+                        frenchVersion.Add("violet");
+                        break;
+                    case "red":
+                        frenchVersion.Add("rouge");
+                        break;
+                    case "white":
+                        frenchVersion.Add("blanc");
+                        break;
+                    case "yellow":
+                        frenchVersion.Add("jaune");
+                        break;
+                  
+                }
+            }
+        }
+
+    }
+    private void FillWordArray()//specify which words to fetch from list
+    {
+        StreamReader reader = new StreamReader(path);
+        string fileContent = reader.ReadToEnd();
+        //Debug.Log(fileContent);
+        reader.Close();
+        string[] temp = fileContent.Split(';');
+        for (int i = 0; i < temp.Length; i++)
+        {
+            if (i % 2 == 0)
+            {
+                englishVersion.Add(temp[i]);
+            }
+            else
+            {
+                frenchVersion.Add(temp[i]);
+            }
+        }
+        Debug.Log("done");
+
+    }
+    private void FetchContent(int theIndex)
+    {
+
+        string theURL = URL + frenchVersion[theIndex] + "&image_type=photo";
+
+        StartCoroutine(GetRequest(theURL, theIndex));
+    }
+    private IEnumerator GetRequest(string uri, int i)
+    {
+
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
+        {
+            // Request and wait for the desired page.
+            yield return webRequest.SendWebRequest();
+            if (webRequest.isNetworkError)
+            {
+
+            }
+            else if (!webRequest.isHttpError)
+            {
+                string[] pageContent = webRequest.downloadHandler.text.Split('"');
+                for (int x = 0; x < pageContent.Length; x++)
+                {
+
+                    if (pageContent[x].Equals("largeImageURL"))
+                    {
+
+                        imgURL = pageContent[x + 2];
+                        Debug.Log(imgURL + " " + frenchVersion[i]);
+                        StartCoroutine(GetTexture(imgURL));
+                        break;
+                    }
+                }
+
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    public void GenerateAWord()
-    {
-        englishWord.text = "Dog";
-        frenchWord.text = "Chien";
 
-        PlayerPrefs.SetString("theWord", frenchWord.text);
-        
-        int index = Random.Range(0, 3);
-        Debug.Log("the index " + index);
-        GenerateRandomWords(index);
-        frameTexture.SetTexture("_MainTex", wordPicture[0]);
-    }
-    private void GenerateRandomWords(int ind)
+    private IEnumerator GetTexture(string img)
     {
-        switch(ind)
+
+        UnityWebRequest www = UnityWebRequestTexture.GetTexture(img);
+        yield return www.SendWebRequest();
+
+        Texture myTexture = DownloadHandlerTexture.GetContent(www);
+       
+        UpdateDisplay(myTexture, frenchVersion[currentIndex], englishVersion[currentIndex]);
+
+
+    }
+    private void UpdateDisplay(Texture theTexture, string french,string english)
+    {
+        frameTexture.mainTexture = theTexture;
+        frenchWord.text = french;
+        englishWord.text = english;
+
+    }
+    public void GenerateRandomWords()//to be called when pressing practice
+    {
+        int ind = Random.Range(0, 3);//chooses which cube will have the correct word
+        int rand1 = Random.Range(0, frenchVersion.Count);
+        int rand2 = Random.Range(0, frenchVersion.Count);
+        if (rand1 == currentIndex || rand1 == rand2)
+        {
+            rand1 = Random.Range(0, frenchVersion.Count);
+        }
+        else if (rand2 == currentIndex)
+        {
+            rand2 = Random.Range(0, frenchVersion.Count);
+        }
+       
+
+        switch (ind)
         {
             case 0:
                 word1.text = frenchWord.text;
-                word2.text ="";
-                word3.text = "";
+                word2.text = frenchVersion[rand1];
+                word3.text = frenchVersion[rand2];
                 break;
             case 1:
-                word1.text = "";
+                word1.text = frenchVersion[rand1];
                 word2.text = frenchWord.text;
-                word3.text = "";
+                word3.text = frenchVersion[rand2];
                 break;
             case 2:
-                word1.text = "";
-                word2.text = "";
+                word1.text = frenchVersion[rand1];
+                word2.text = frenchVersion[rand2];
                 word3.text = frenchWord.text;
                 break;
         }
     }
+    public void NextImage()
+    {
+        if (currentIndex == (frenchVersion.Count - 1))
+        {
+            currentIndex = 0;
+        }
+        else
+        {
+            currentIndex++;
+        }
+        if (PlayerPrefs.GetInt("Level") > 1)
+        {
+            FetchContent(currentIndex);
+        }
+        else
+        {
+            UpdateDisplay(colors[currentIndex], frenchVersion[currentIndex], englishVersion[currentIndex]);
+        }
+        
+
+    }
+    public void PreviousImage()
+    {
+        if (currentIndex == 0)
+        {
+            currentIndex = (frenchVersion.Count - 1);
+        }
+        else
+        {
+            currentIndex--;
+        }
+        if (PlayerPrefs.GetInt("Level") > 1)
+        {
+            FetchContent(currentIndex);
+        }
+        else
+        {
+            UpdateDisplay(colors[currentIndex], frenchVersion[currentIndex], englishVersion[currentIndex]);
+        }
+    }
+
+
+
 }
--- MSP-HandTracking/Assets/Script/resetDesk.cs
+++ MSP-HandTracking/Assets/Script/resetDesk.cs
@@ -5,28 +5,24 @@ using UnityEngine;
 public class resetDesk : MonoBehaviour
 {
     private Vector3 initialPosW1,initialPosW2,initialPosW3;
-    private Quaternion initialRotXW1;//,initialRotYW1,initialRotZW1;//word1 rotation
-    private Quaternion initialRotXW2;//,initialRotYW2,initialRotZW2;//word2 rotation
-    private Quaternion initialRotXW3;//,initialRotYW3,initialRotZW3;//word3 rotation
+    private Quaternion initialRotXW1;
+    private Quaternion initialRotXW2;
+    private Quaternion initialRotXW3;
    
-    private Quaternion W1Rot, W2Rot, W3Rot;
+
     public GameObject word1, word2, word3;
-    // Start is called before the first frame update
+
     void Start()
     {
        
         initialRotXW1 = word1.transform.localRotation;
-        //initialRotYW1 = word1.transform.localRotation.y;
-        //initialRotZW1 = word1.transform.localRotation.z;
+
 
         initialRotXW2 = word2.transform.localRotation;
-       // initialRotYW2 = word2.transform.localRotation.y;
-       // initialRotZW2 = word2.transform.localRotation.z;
+
 
         initialRotXW3 = word3.transform.localRotation;
-        // initialRotYW3 = word3.transform.localRotation.y;
-        // initialRotZW3 = word3.transform.localRotation.z;
-        Debug.Log("Word1 " + initialRotXW1 + " word2 " + initialRotXW2 + " word3" + initialRotXW3);
+      
         initialPosW1 = word1.transform.position;
         initialPosW2 = word2.transform.position;
         initialPosW3 = word3.transform.position;
